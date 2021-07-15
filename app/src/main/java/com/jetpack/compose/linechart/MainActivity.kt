09d2d8ec766ef4c2fbc1df4d5c891ffe86b9169f
2161783757@qq.com <43898347+wdfHPY@users.noreply.github.com>
2021-07-15 19:55:49 +0800

    build.gradle MainActivity.kt

--- app/src/main/java/com/jetpack/compose/linechart/MainActivity.kt
+++ app/src/main/java/com/jetpack/compose/linechart/MainActivity.kt
@@ -1,18 +1,36 @@
 package com.jetpack.compose.linechart
 
 import android.os.Bundle
+import android.view.MotionEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalComposeUiApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,13 +39,42 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview
+@ExperimentalComposeUiApi
+@Preview(widthDp = 1000, heightDp = 1000)
 @Composable
 fun DefaultPreview() {
-    Box(modifier = Modifier.wrapContentSize()) {
-        Text(text = "LineChart", fontSize = with(LocalDensity.current) {
-            10.dp.toSp()
-        })
+    val xPosition = remember {
+        mutableStateOf<Float>(0.0F)
     }
 
+    val yPosition = remember {
+        mutableStateOf<Float>(0.0F)
+    }
+    val path = Path()
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Canvas(modifier = Modifier
+            .fillMaxSize()
+            .pointerInteropFilter {
+                when (it.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        xPosition.value = it.x - 30
+                        yPosition.value = it.y - 154
+                    }
+                    else -> false
+                }
+                true
+            }
+        ) {
+            drawRoundRect(
+                color = Color.White,
+                topLeft = Offset(xPosition.value, yPosition.value),
+                cornerRadius = CornerRadius(x = 20F, y = 20F),
+                size = Size(500F, 120F)
+            )
+            drawPath(
+
+            )
+        }
+
+    }
 }
